8130db4e2259cd6a014bb0ab049625bfd7812534
Chris Andrejewski <christopher.andrejewski@gmail.com>
2015-05-12 18:26:46 -0400

    Real initial commit
    Ya got ya parser and ya tests and ya README

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# himalaya
-HTML -> JSON parser
+# Himalaya
+
+Himalaya is a pure JavaScript HTML parser that converts HTML into JSON, which can then be further manipulated by other modules.
+
+```bash
+npm install himalaya
+```
+
+## Usage
+
+```javascript
+var himalaya = require('himalaya');
+var html = require('fs').readFileSync('/webpage.html');
+var json = himalaya.parse(html);
+```
+
+## Input/Output
+
+```html
+<div class='post post-featured'>
+	<p>Himalaya parsed me...</p>
+	<!-- ...and I liked it. -->
+</div>
+```
+
+```json
+[
+  {
+    "className": [
+      "post",
+      "post-featured"
+    ],
+    "tagName": "div",
+    "children": [
+      {
+        "tagName": "p",
+        "children": [
+          {
+            "content": "Himalaya parsed me...",
+            "kind": "text"
+          }
+        ],
+        "kind": "element"
+      },
+      {
+        "content": " ...and I liked it. ",
+        "kind": "comment"
+      }
+    ],
+    "kind": "element"
+  }
+]
+```
+
+*Note*: Text nodes containing only whitespace were removed from the output for readability.
+
+## Features
+
+### Synchronous 
+HTML is turned into JSON, that's it. Himalaya is completely synchronous and does not require any complicated callbacks. 
+
+### Parses Attributes
+Himalaya does a couple things when processing attributes:
+- Camel-cases attribute names
+- Attributes without values are set to their names (i.e. `disabled` turns into `disabled="disabled"`)
+- Groups `data-*` attributes into a `dataset` object
+- Attempts to caste any value to a number if `!Nan`
+- Parses the `style` attribute into a hash map
+
+### Handles Weirdness
+Himalaya handles a lot of HTML's fringe cases, like:
+- Closes unclosed tags `<p><b>...</p>`
+- Ignores extra closing tags `<span>...</b></span>`
+- Properly handles void tags like `<meta>` and `<img>`
+- Properly handles self-closing tags like `<input/>`
+- Handles `<!doctype>` and `<-- comments -->`
+- Does not parse the contents of `<script>`, `<style>`, and HTML5 `<template>` tags
+
+### Preserves Whitespace
+Himalaya does not cut corners and returns an accurate representation of the HTML supplied.
+
+## Why "Himalaya"?
+
+[First, my friends weren't very helpful.](https://twitter.com/compooter/status/597908517132042240) Except Josh, Josh had my back.
+
+While I was testing the parser, I threw a download of my Twitter homepage in and got a huge JSON blob out. My code editor Sublime Text has a mini-map and looking at it sideways the data looked like a never-ending mountain range. Also, "himalaya" has H, M, L in it.
+
+## Nerd Stuff
+
+My first implementation used look-ahead, found the matching closing tag, and then recursively parsed the inners until a tree was built. That was problematic. This implementation uses no look-ahead and instead uses a stack to keep track of nesting as the source string is cut away. When an end tag is found the stack it cut to match and then parsing picks up again at the higher level. 
+
+This parser was implemented without using any regular expressions. Since I wanted this to double as a learning resource, I wanted the code to be readable sans regexes.
+
+## Contributing
+
+We can always have more tests: if you find a bug, create an issue or be **fabulous** and fix the problem and write the tests up yourself in a coherent pull request.
+
+Run tests with the `npm test` command.
+
+Follow me on [Twitter](https://twitter.com/compooter) for updates or just for the lolz and please check out my other [repositories](https://github.com/andrejewski) if I have earned it. I thank you for reading.
---
+++ index.js
@@ -0,0 +1,212 @@
+
+var tagStart = '<';
+var tagEnd = '>';
+
+var commentStart = '<!--';
+var commentEnd = '-->';
+
+var voidTags = [
+	"!doctype", "area", "base", "br", "col", "command", 
+	"embed", "hr", "img", "input", "keygen", "link", 
+	"meta", "param", "source", "track", "wbr"];
+var closingTags = [
+	"colgroup", "dd", "dt", "li", "options", "p", 
+	"td", "tfoot", "th", "thead", "tr"];
+var childlessTags = ['style', 'script', 'template'];
+
+function parse(str) {
+	return parseUntil(str+'</root>', ['root']).nodes;
+}
+
+function Node(kind, data) {
+	data.kind = kind;
+	return data;
+}
+
+function parseUntil(str, stack) {
+	var nodes = [];
+
+	while(str.length) {
+		var nextTag = str.indexOf(tagStart);
+		if(!~nextTag) {
+			// only text left
+			nodes.push(Node('text', {content: str}));
+			str = "";
+		} else if(nextTag) {
+			// text before tag
+			nodes.push(Node('text', {
+				content: str.slice(0, nextTag)
+			}));
+			str = str.slice(nextTag);
+		} else {
+			if(!str.indexOf(commentStart)) {
+				// comment
+				var end = str.indexOf(commentEnd);
+				nodes.push(Node('comment', {
+					content: str.slice(commentStart.length, end)
+				}));
+				str = str.slice(end + commentEnd.length);
+			} else if(str.charAt(nextTag + 1) !== '/') {
+				// open tag
+				var results = parseTag(str, stack);
+				if(results.tag) {
+					nodes.push(Node('element', results.tag));
+					str = results.str;
+				}
+				if(results.stack.length !== stack.length) {
+					stack = results.stack;
+					break;
+				}
+			} else {
+				// close tag
+				var endTagEnd = str.indexOf(tagEnd);
+				var tagName = str.slice(2, endTagEnd)
+					.trim().split(' ')[0];
+				str = str.slice(endTagEnd + 1);
+				var loc = stack.lastIndexOf(tagName);
+				if(~loc) {
+					stack = stack.slice(0, loc);
+					break;
+				}
+			}
+		}
+	}
+
+	return {nodes: nodes, stack: stack, str: str};
+}
+
+function parseTag(str, stack) {
+	var idxTagEnd = str.indexOf(tagEnd);
+	var idxSpace = str.indexOf(' ');
+	var tagNameEnd = ~idxSpace 
+		? Math.min(idxTagEnd, idxSpace)
+		: idxTagEnd;
+	var tagName = str.slice(1, tagNameEnd);
+	var lowTagName = tagName.toLowerCase();
+
+	if(stack[stack.length - 1] === tagName && ~closingTags.indexOf(lowTagName)) {
+		return {stack: stack.slice(0, -1)};
+	}
+
+	var attrs = parseAttrs(str.slice(tagNameEnd));
+	var tag = attrs.attributes;
+	str = attrs.str;
+
+	tag.tagName = tagName;
+	if(!str.indexOf('/>')) {
+		str = str.slice(2);
+	} else {
+		str = str.slice(1);
+		
+		if(~childlessTags.indexOf(lowTagName)) {
+			var end = '</'+tagName+'>';
+			var idx = str.indexOf(end);
+			if(!~idx) {
+				throw new Error('<'+tagName+'> missing closing '+end+' tag.');
+			}
+			tag.content = str.slice(0, idx);
+			str = str.slice(idx);
+		} else if(!~voidTags.indexOf(lowTagName)) {
+			var results = parseUntil(str, stack.concat(tagName));
+			tag.children = results.nodes;
+			str = results.str;
+			stack = results.stack;
+		}
+	}
+
+	return {tag: tag, str: str, stack: stack};
+}
+
+function parseAttrs(str) {
+	var results = tagPairs(str.trim());
+	var str = results.str;
+	var attributes = results.kvs.map(function(pair) {
+		var kv = splitHead(pair.trim(), '=');
+		kv[1] = kv[1] ? unquote(kv[1]) : kv[0];
+		return kv;
+	}).reduce(function(attrs, kv) {
+		var property = kv[0];
+		var value = kv[1];
+		if(property === 'class') {
+			attrs.className = value.split(' ');
+		} else if(property === 'style') {
+			attrs.style = parseStyle(value);
+		} else if (!property.indexOf('data-')) {
+			attrs.dataset = attrs.dataset || {};
+			var key = camelCase(property.slice(5));
+			attrs.dataset[key] = castValue(value);
+		} else {
+			attrs[camelCase(property)] = castValue(value);
+		}
+		return attrs;
+	}, {});
+	return {str: str, attributes: attributes};
+}
+
+function splitHead(str, sep) {
+	var idx = str.indexOf(sep);
+	if(!~idx) return [str];
+	return [str.slice(0, idx), str.slice(idx + sep.length)];
+}
+
+function tagPairs(str) {
+	var parts = [];
+	var start = 0;
+	var finish = 0;
+	var quote = null;
+	for(var i = start; i < str.length; i++) {
+		var c = str.charAt(i);
+		if(!quote && (c === '/' || c === tagEnd)) {
+			if(start !== i) parts.push(str.slice(start, i));
+			finish = i;
+			break;
+		} else if(c === ' ' && !quote) {
+			parts.push(str.slice(start, i));
+			start = i + 1;
+		} else if(c === quote) {
+			quote = null;
+		} else if(!quote && (c === '"' || c === "'")) {
+			quote = c;
+		}
+	}
+	return {kvs: parts, str: str.slice(finish)};
+}
+
+function unquote(str) {
+	var car = str.charAt(0);
+	var end = str.length - 1;
+	if(car === '"' || car === "'" && car === str.charAt(end)) {
+		return str.slice(1, end);
+	}
+	return str;
+}
+
+function parseStyle(str) {
+	return str.trim().split(';').map(function(statement) {
+		return statement.trim().split(':');
+	}).reduce(function(styles, kv) {
+		if(kv[1]) styles[camelCase(kv[0].trim())] = castValue(kv[1].trim());
+		return styles;
+	}, {});
+}
+
+function camelCase(str) {
+	return str.split('-').reduce(function(str, word) {
+		return str + word.charAt(0).toUpperCase() + word.slice(1);
+	});
+}
+
+function castValue(str) {
+	if(typeof str !== 'string') return str;
+	var num = +str;
+	if(!isNaN(num)) return num;
+	return str;
+}
+
+module.exports = {
+	parse: parse,
+	parseTag: parseTag,
+	parseUntil: parseUntil,
+	parseAttrs: parseAttrs,
+	parseStyle: parseStyle,
+}
